835077e110ea06697e299c5f973d8dd6f9baaaa6
lzycc234 <lzycc234@outlook.com>
2018-02-06 01:04:54 +0800

    renew

--- QGettingStart/QGettingStart.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "QGSAbstractGame.h"
-#include "QGSAbstractGameBuilder.h"
-#include "QGSAbstractLauncher.h"
-
-#include "QGSGame.h"
-#include "QGSGameBuilder.h"
-#include "QGSLauncher.h"
--- QGettingStart/QGettingStart/Alpha 1.2.0/UTF-8/QGettingStart.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Game.h"
-#include "GameBuilder.h"
-#include "Launcher.h"
-
-#include "QGSGame.h"
-#include "QGSGameBuilder.h"
-#include "QGSLauncher.h"
